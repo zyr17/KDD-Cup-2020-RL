ce49e819f3e50254a23a35b9aa67fb7307d1c2e0
zyr17 <jzjqz17@gmail.com>
2020-05-18 13:31:09 +0800

    update

--- run_local.sh
+++ run_local.sh
@@ -9,4 +9,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 docker build -t kddcup -f Dockerfile .
-docker run -it kddcup --rm
+docker run -it --rm -v `pwd`:/app kddcup
